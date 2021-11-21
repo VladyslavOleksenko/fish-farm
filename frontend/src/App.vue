933ac53b0a8c8886fa3ec074bffec0b5cdf187a0
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-21 11:25:30 +0200

    FRONT: base app styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,32 @@
 <template>
-  Hello from vue
+  <Sidebar/>
+  <div :style="{ 'margin-left:': sidebarWidth }">
+    <router-view/>
+  </div>
 </template>
 
+<script>
+import Sidebar from "@/components/Sidebar/Sidebar";
+import {sidebarWidth} from "@/components/Sidebar/state";
+
+export default {
+  components: {Sidebar},
+  setup() {
+    return {sidebarWidth}
+  }
+}
+</script>
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Montserrat', Arial, sans-serif;
+}
 </style>
