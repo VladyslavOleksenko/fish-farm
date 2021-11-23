9ea199ab1e0c4bc5df8624a307820df0db90a0a1
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-23 16:09:57 +0200

    FRONT: create Authorization view

---
+++ frontend/src/components/AuthorizationBlock/AuthorizationBlock.vue
@@ -0,0 +1,196 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper__switcher switcher">
+      <div class="switcher__brick"
+           :class="{'switcher__brick-right': mode === 'login'}"></div>
+      <button class="switcher__button"
+              @click="mode = 'register'">
+        Register
+      </button>
+      <button class="switcher__button switcher__button-right"
+              @click="mode = 'login'">
+        Login
+      </button>
+    </div>
+
+    <form class="wrapper__register-form form"
+          action="http://localhost:5000/api/user"
+          method="post"
+          v-if="mode === 'register'">
+      <div class="form__input-fields-row">
+        <input class="form__input" type="text" placeholder="First name">
+        <input class="form__input" type="text" placeholder="Last name">
+      </div>
+      <div class="form__input-fields-row">
+        <input class="form__input" type="email" placeholder="Email">
+      </div>
+      <div class="form__input-fields-row">
+        <input class="form__input" type="password" placeholder="Password">
+      </div>
+
+      <div class="form__submit-wrapper">
+        <button class="form__submit">
+          Register
+        </button>
+      </div>
+    </form>
+
+    <form class="wrapper__login-form form"
+          action=""
+          v-else-if="mode === 'login'">
+      <div class="form__input-fields-row">
+        <input class="form__input" type="email" placeholder="Email">
+      </div>
+      <div class="form__input-fields-row">
+        <input class="form__input" type="password" placeholder="Password">
+      </div>
+
+      <div class="form__submit-wrapper">
+        <button class="form__submit"
+                disabled
+                @click.prevent>
+          Login
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthorizationBlock",
+  data: () => ({
+    mode: "register"
+  })
+}
+</script>
+
+<style scoped>
+.wrapper {
+  width: 30vw;
+
+  background-color: var(--dark-purple-color);
+  border-radius: 6px;
+}
+
+
+.wrapper__switcher {
+  margin: 0 0 50px 0;
+}
+
+.switcher {
+  position: relative;
+  height: 60px;
+
+  display: flex;
+}
+
+.switcher__brick {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50%;
+  height: 100%;
+
+  background-color: var(--blue-color);
+  border-radius: 6px 0 0 0;
+
+  transition: left .2s ease,
+  border-radius .2s ease;
+}
+
+.switcher__brick-right {
+  border-radius: 0 6px 0 0;
+  left: 50%;
+}
+
+.switcher__button {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50%;
+  height: 100%;
+
+  font-size: 27px;
+  font-weight: 500;
+
+  color: #eee;
+  background: none;
+  border: none;
+  outline: none;
+
+  cursor: pointer;
+  z-index: 1;
+}
+
+.switcher__button-right {
+  left: 50%;
+}
+
+
+.form__input-fields-row {
+  margin: 0 0 22px 0;
+  padding: 0 20px;
+  width: calc(100% + 15px);
+
+  display: flex;
+}
+
+.form__input {
+  width: 100%;
+  height: 53px;
+  margin: 0 15px 0 0;
+  padding: 0 15px;
+
+  font-size: 22px;
+
+  color: #fff;
+  background-color: var(--light-purple-color);
+  border: none;
+  border-radius: 4px;
+  outline: none;
+}
+
+.form__input::placeholder {
+  color: var(--light-gray-color);
+}
+
+
+.form__submit-wrapper {
+  margin: 50px 0 20px;
+
+  display: flex;
+  justify-content: center;
+}
+
+.form__submit {
+  height: 50px;
+  padding: 0 50px;
+
+  font-size: 22px;
+  font-weight: 500;
+
+  color: #eee;
+  background-color: var(--blue-color);
+
+  border: none;
+  border-radius: 4px;
+  outline: none;
+
+  transition: background-color .2s ease;
+  cursor: pointer;
+}
+
+.form__submit:hover {
+  background-color: #6fa360;
+}
+
+.form__submit:active {
+  background-color: #5e8b52;
+}
+
+.form__submit[disabled] {
+  color: #7f7f7f;
+  background: var(--light-purple-color);
+}
+</style>
---
+++ frontend/src/views/Authorization.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="authorization">
+    <img class="authorization__greeting-image" src="../../public/lamp.svg" alt="">
+    <AuthorizationBlock class="authorization__form-block"/>
+
+    <div class="authorization__page-title">
+      <span class="authorization__greeting">Welcome </span>
+      <span>to </span>
+      <span class="authorization__app-name">FISH Farm</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyIcon from "@/components/UI/MyIcon";
+import AuthorizationBlock from "@/components/AuthorizationBlock/AuthorizationBlock";
+
+export default {
+  name: "Authorization",
+  components: {AuthorizationBlock, MyIcon},
+  data: () => ({
+    mode: "register"
+  }),
+}
+</script>
+
+
+<style>
+:root {
+  --dark-purple-color: #2C2E43;
+  --light-purple-color: #595260;
+  --blue-color: #494b6c;
+  --light-gray-color: #B2B1B9;
+
+}
+</style>
+
+<style scoped>
+.authorization {
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+
+  background-color: var(--light-gray-color);
+}
+
+
+.authorization__page-title {
+  padding: 2vw 0 0 5vw;
+
+  font-size: 40px;
+
+  color: var(--dark-purple-color)
+}
+
+.authorization__greeting, .authorization__app-name {
+  font-weight: 500;
+  font-size: 50px;
+}
+
+
+.authorization__greeting-image {
+  position: absolute;
+  top: 30%;
+  left: 10%;
+  transform: scale(-1, 1);
+  height: 60%;
+
+  opacity: .8;
+}
+
+.authorization__form-block {
+  position: absolute;
+  right: 10%;
+  top: 30%;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
