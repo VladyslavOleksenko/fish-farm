3403ccdfccd10b28d3023765f3b2fd7ea58c8581
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-23 17:18:15 +0200

    FRONT: create RequestParams class

---
+++ frontend/src/assets/js/RequestParams.js
@@ -0,0 +1,10 @@
+export default class RequestParams {
+  constructor(method = "GET", url = "/", body = null) {
+    this.method = method;
+    this.url = url;
+    this.body = body;
+    this.responseType = "json";
+    this.contentType = "application/json";
+    this.stringify = true;
+  }
+}
