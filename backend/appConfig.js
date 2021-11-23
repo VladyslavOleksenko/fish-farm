798b641faef6d2f44a0d224c7956c5f64d975287
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-23 20:57:11 +0200

    BACK: update appConfig.js
    - add dataBaseCondig
    - add private key for jsonwebtoken

--- backend/appConfig.js
+++ backend/appConfig.js
@@ -1,5 +1,12 @@
 module.exports = {
   server: {
     PORT: 5000
-  }
+  },
+  dataBaseConfig: {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'fish_farm'
+  },
+  jsonwebtokenKey: "1234"
 }
