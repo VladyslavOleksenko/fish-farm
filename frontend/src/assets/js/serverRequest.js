7aecf7f9becee43224760ea39b9032511589b3e2
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-23 17:18:36 +0200

    FRONT: create serverRequest.js

---
+++ frontend/src/assets/js/serverRequest.js
@@ -0,0 +1,57 @@
+import RequestParams from "@/assets/js/RequestParams";
+
+async function sendServerRequest(requestParams) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(requestParams.method, requestParams.url);
+
+    xhr.responseType = requestParams.responseType;
+    xhr.setRequestHeader("Content-Type", requestParams.contentType);
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response)
+      } else {
+        resolve(xhr.response);
+      }
+    }
+
+    xhr.onerror = () => {
+      reject(xhr.response);
+    }
+
+    if (requestParams.stringify) {
+      requestParams.body = JSON.stringify(requestParams.body);
+    }
+
+    xhr.send(requestParams.body);
+  });
+}
+
+
+export async function register(body) {
+  const requestParams = new RequestParams(
+    "GET",
+    "http://localhost:5000/api/user",
+    null
+  )
+  requestParams.responseType = "text"
+
+  console.log("request params:", requestParams)
+
+  return await sendServerRequest(requestParams)
+}
+
+export async function login(body) {
+  const requestParams = new RequestParams(
+    "GET",
+    "http://localhost:5000/api/user",
+    null
+  )
+  requestParams.responseType = "text"
+
+  console.log("request params:", requestParams)
+
+  return await sendServerRequest(requestParams)
+}
