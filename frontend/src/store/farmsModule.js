ae7fc97cfe7164a6fa7762e4e3b528e02aa0ed5f
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-24 18:19:43 +0200

    FRONT: update farmsModule.js
    get ownFarms from server

--- frontend/src/store/farmsModule.js
+++ frontend/src/store/farmsModule.js
@@ -1,34 +1,28 @@
+import {getOwnFarms} from "@/assets/js/serverRequest";
+
 export default {
   namespaced: true,
   state: () => ({
-    ownFarms: [
-      {
-        farmId: 1,
-        name: "farm 1",
-        description: "It is farm 1",
-        owner_id: "1"
-      },
-      {
-        farmId: 2,
-        name: "farm 2",
-        description: "It is farm 2",
-        owner_id: "2"
-      },
-      {
-        farmId: 3,
-        name: "farm 3",
-        description: "It is farm 3",
-        owner_id: "3"
-      },
-      {
-        farmId: 4,
-        name: "farm 4",
-        description: "It is farm 4",
-        owner_id: "4"
-      },
-    ]
+    ownFarms: []
   }),
   getters: {},
-  mutations: {},
-  actions: {}
+  mutations: {
+    setOwnFarms(state, ownFarms) {
+      state.ownFarms = ownFarms
+    }
+  },
+  actions: {
+    async updateOwnFarms({state, commit}, userId) {
+      try {
+        const serverResponse = await getOwnFarms(userId)
+        if (serverResponse.farms && serverResponse.farms.length) {
+          return commit('setOwnFarms', serverResponse.farms)
+        }
+        commit('setOwnFarms', [])
+      } catch (exception) {
+        console.log("Can't update own farms")
+        console.log(exception)
+      }
+    }
+  }
 }
