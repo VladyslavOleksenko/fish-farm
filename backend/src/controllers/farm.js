1009cdfcc9571dd7f0964d9290c4d97478e98b4d
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-24 18:18:18 +0200

    BACK: update farm.js
    getOwnFarms request

--- backend/src/controllers/farm.js
+++ backend/src/controllers/farm.js
@@ -1,13 +1,62 @@
 const express = require("express")
 const {createInsertSqlCommand, sendDataBaseQuery} = require("./../dataBase");
+const {findUserByUserId} = require("./user")
 
 const router = express.Router()
 router.get("/ownFarms", getOwnFarms)
 
 
 async function getOwnFarms(request, response) {
-  console.log("/api/ownFarms requested")
-  response.json({message: "/api/ownFarms requested"})
+  try {
+    const owner = await findUserByUserId(request.query.userId)
+    if (!owner) {
+      return throwError(`No user with id ${request.query.userId}`)
+    }
+
+    const farms = await findFarmsByOwnerId(owner["user_id"])
+
+    response.json({farms})
+  } catch (exception) {
+    response.status(500).json({message: exception})
+    throwError("Can't get own farms")
+    throwError(exception)
+  }
+}
+
+async function findFarmsByOwnerId(ownerId) {
+  const sqlCommand = `SELECT *
+                      FROM farm
+                      WHERE owner_id LIKE '${ownerId}'`
+  const dataBaseResponse = await sendDataBaseQuery(sqlCommand)
+
+  if (dataBaseResponse && dataBaseResponse.rows && dataBaseResponse.rows.length) {
+    return formatFarmArray(dataBaseResponse.rows)
+  }
+  return null
+}
+
+
+function formatFarmArray(farmArray) {
+  let farmArrayFormatted = []
+  for (let farm of farmArray) {
+    farmArrayFormatted.push(formatFarm(farm))
+  }
+
+  return farmArrayFormatted
+}
+
+function formatFarm(farm) {
+  return {
+    farmId: farm["farm_id"],
+    name: farm.name,
+    description: farm.description,
+    ownerId: farm["owner_id"]
+  }
+}
+
+
+function throwError(error) {
+  console.log("Error: ", error)
 }
 
 
