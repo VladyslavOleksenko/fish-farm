f5ca1ae1d02d388203f5cdf6e654e778b8c526a6
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-24 14:09:39 +0200

    FRONT: update farmsModule.js
    change ownFarms.id to ownFarms.farmId

--- frontend/src/store/farmsModule.js
+++ frontend/src/store/farmsModule.js
@@ -1,27 +1,27 @@
 export default {
   namespaced: true,
   state: () => ({
-    farms: [
+    ownFarms: [
       {
-        id: 1,
+        farmId: 1,
         name: "farm 1",
         description: "It is farm 1",
         owner_id: "1"
       },
       {
-        id: 2,
+        farmId: 2,
         name: "farm 2",
         description: "It is farm 2",
         owner_id: "2"
       },
       {
-        id: 3,
+        farmId: 3,
         name: "farm 3",
         description: "It is farm 3",
         owner_id: "3"
       },
       {
-        id: 4,
+        farmId: 4,
         name: "farm 4",
         description: "It is farm 4",
         owner_id: "4"
