640926a233679b2c4dba6a7d11e78e405496d2a0
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-24 11:58:04 +0200

    FRONT: update Home.vue
    - add Switcher
    - add FarmListPlug
    - add FarmList

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,52 @@
 <template>
-
+  <div class="home">
+    <Switcher class="home__switcher"
+              :left-option="'my own farms'"
+              :right-option="'other farms'"
+              @option-selected="switcherOptionSelected"/>
+    <FarmListPlug class="home__own-farms-plug"
+                  :farms-filter="farmsFilter"/>
+    <FarmList/>
+  </div>
 </template>
 
 <script>
+import Switcher from "@/components/Switcher/Switcher";
+import FarmList from "@/components/FarmList/FarmList";
+import FarmListPlug from "@/components/FarmList/FarmListPlug";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {FarmListPlug, FarmList, Switcher},
+  data: () => ({
+    farmsFilter: "own"
+  }),
+  methods: {
+    switcherOptionSelected(selectedOptionPosition) {
+      this.farmsFilter = selectedOptionPosition === "left" ? "own" : "other"
+    }
+  }
 }
 </script>
 
 <style scoped>
+.home {
+  padding: 30px 0 0 0;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+
+.home__switcher {
+  margin: 0 0 30px 0;
+  width: 35vw;
+  height: 60px;
+}
+
+
+.home__own-farms-plug {
+  width: 90%;
+}
 </style>
