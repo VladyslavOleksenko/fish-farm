a946a1477d0ea7b440b8830096ddecde521a4b8f
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-24 15:16:55 +0200

    FRONT: update userModule

--- frontend/src/store/userModule.js
+++ frontend/src/store/userModule.js
@@ -5,24 +5,15 @@ export default {
   }),
   mutations: {
     setUser(state, user) {
-      if (!user ||
-        !user["user_id"] ||
-        !user.email ||
-        !user["first_name"] ||
-        !user["last_name"]
+      if (!user || !user.userId || !user.email || !user.firstName ||
+        !user.lastName
       ) {
         console.log("cant set user to storage from vuex")
         return
       }
 
-      const newUserObject = {
-        userId: user["user_id"],
-        email: user.email,
-        firstName: user["first_name"],
-        lastName: user["last_name"]
-      }
-      state.user = newUserObject
-      localStorage.user = JSON.stringify(newUserObject)
+      state.user = user
+      localStorage.user = JSON.stringify(user)
     }
   }
 }
