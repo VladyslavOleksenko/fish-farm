361e2fedb065daf1a8b1dd983c780f4a43c08433
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-25 09:05:21 +0200

    BACK: getFarm request

--- backend/src/controllers/farm.js
+++ backend/src/controllers/farm.js
@@ -3,6 +3,7 @@ const {createInsertSqlCommand, sendDataBaseQuery} = require("./../dataBase");
 const {findUserByUserId} = require("./user")
 
 const router = express.Router()
+router.get("/", getFarm)
 router.get("/ownFarms", getOwnFarms)
 router.post("/create", createFarm)
 
@@ -11,7 +12,9 @@ async function getOwnFarms(request, response) {
   try {
     const owner = await findUserByUserId(request.query.userId)
     if (!owner) {
-      return throwError(`No user with id ${request.query.userId}`)
+      const errorMessage = `No user with id ${request.query.userId}`
+      response.status(404).json({message: errorMessage})
+      return throwError(errorMessage)
     }
 
     const farms = await findFarmsByOwnerId(owner["user_id"])
@@ -28,7 +31,9 @@ async function createFarm(request, response) {
   try {
     const user = await findUserByUserId(request.body.userId)
     if (!user) {
-      return throwError(`No user with id ${request.query.userId}`)
+      const errorMessage = `No user with id ${request.body.userId}`
+      response.status(404).json({message: errorMessage})
+      return throwError(errorMessage)
     }
 
     const tableName = "farm"
@@ -57,6 +62,23 @@ async function createFarm(request, response) {
   }
 }
 
+async function getFarm(request, response) {
+  try {
+    const farm = await findFarmById(request.query.farmId)
+    if (!farm) {
+      const errorMessage = `No farm with id ${request.query.farmId}`
+      response.status(404).json({message: errorMessage})
+      throwError(errorMessage)
+    }
+
+    response.json({farm})
+  } catch (exception) {
+    response.status(500).json({message: exception})
+    throwError("Can't get farm")
+    throwError(exception)
+  }
+}
+
 
 async function findFarmsByOwnerId(ownerId) {
   const sqlCommand = `SELECT *
@@ -69,6 +91,20 @@ async function findFarmsByOwnerId(ownerId) {
   }
   return null
 }
+
+async function findFarmById(farmId) {
+  const sqlCommand = `SELECT *
+                      FROM farm
+                      WHERE farm_id LIKE '${farmId}'`
+  const dataBaseResponse = await sendDataBaseQuery(sqlCommand)
+
+  if (dataBaseResponse && dataBaseResponse.rows && dataBaseResponse.rows.length) {
+    return formatFarm(dataBaseResponse.rows[0])
+  }
+  return null
+}
+
+
 
 
 function formatFarmArray(farmArray) {
