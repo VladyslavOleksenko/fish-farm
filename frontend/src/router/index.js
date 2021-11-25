ba13b2e6eec53abe837ec062e77b6cd533147b44
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-25 09:06:26 +0200

    FRONT: add farm/:farmId route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -22,6 +22,12 @@ const routes = [
     component: () => import("@/views/Profile")
   },
   {
+    path: "/farm/:farmId",
+    name: "Farm",
+    component: () => import("@/views/Farm"),
+    params: true
+  },
+  {
     path: "/logout",
     name: "Logout",
     redirect: "/"
