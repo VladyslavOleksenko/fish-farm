bfc31c9b3334285107485f9510d61d2da9a09d48
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-25 19:44:10 +0200

    FRONT: create roundButton component

---
+++ frontend/src/components/UI/MyRoundButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="my-round-button">
+    <MyIcon class="my-round-button__icon" :icon-name="iconName" path-color="#eee"/>
+  </div>
+</template>
+
+<script>
+import MyIcon from "@/components/UI/MyIcon";
+
+export default {
+  name: "MyRoundButton",
+  components: {MyIcon},
+  props: {
+    iconName: {type: String, required: true}
+  }
+}
+</script>
+
+<style scoped>
+.my-round-button {
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 2px solid #eeeeee;
+  border-radius: 1000px;
+
+  cursor: pointer;
+  transition: background-color .2s ease;
+}
+
+.my-round-button:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+.my-round-button:active {
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.my-round-button__icon {
+  width: 40%;
+  height: 40%;
+}
+</style>
