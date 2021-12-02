fccb42f68b20a6585d462efb5b8dfef6db695794
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-12-02 16:10:34 +0200

    FRONT: create Tasks & Task components

---
+++ frontend/src/components/TaskBlock/Task.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="task">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/TaskBlock/Tasks.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tasks">
+    <div class="tasks__content">
+      <div class="tasks__list">
+        <Task/>
+        <Task/>
+      </div>
+      <MyRectangleButton
+        class="tasks__add-button"
+        text="Add task"
+        icon-name="add"
+        @click="addTaskModalData.visibilityStatus = true"/>
+    </div>
+
+    <AddTaskModal
+      v-model:visibility-status="addTaskModalData.visibilityStatus"
+      v-model:content="addTaskModalData.content"/>
+  </div>
+</template>
+
+<script>
+import Task from "@/components/TaskBlock/Task";
+import MyRectangleButton from "@/components/UI/MyRectangleButton";
+import AddTaskModal from "@/components/Modal/AddTaskModal";
+
+export default {
+  name: "Tasks",
+  components: {AddTaskModal, MyRectangleButton, Task},
+  props: {
+    farmWorkerId: {type: Number, required: true}
+  },
+  data: () => ({
+    addTaskModalData: {
+      visibilityStatus: false,
+      content: {
+        farmWorkerId: null,
+        title: "",
+        description: "",
+        deadlineDate: "",
+        deadlineTime: "",
+        isRecurring: false,
+        recurringPeriod: "",
+        resultRequiredStatus: false
+      }
+    }
+  }),
+  mounted() {
+    this.addTaskModalData.content.farmWorkerId = this.farmWorkerId
+  }
+}
+</script>
+
+<style scoped>
+
+
+.tasks__add-button {
+  width: 300px;
+  height: 50px;
+}
+</style>
