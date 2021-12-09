5a0e387bd32042d0665a92192aaafb9f77394d13
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-12-09 17:50:17 +0200

    FRONT: parse task history

--- frontend/src/components/TaskBlock/TaskInfo.vue
+++ frontend/src/components/TaskBlock/TaskInfo.vue
@@ -2,64 +2,92 @@
   <div class="task-info">
     <div class="task-info__content"
          v-if="taskInfo">
-      <div class="task-info__worker">
-        <div class="task-info__worker-role">
-          {{ taskInfo.worker.roleName }}
+      <div class="task-info__task">
+        <div class="task-info__worker">
+          <div class="task-info__worker-role">
+            {{ taskInfo.worker.roleName }}
+          </div>
+          <div class="task-info__worker-name">
+            {{ taskInfo.worker.firstName }}
+            {{ taskInfo.worker.lastName }}
+          </div>
         </div>
-        <div class="task-info__worker-name">
-          {{ taskInfo.worker.firstName }}
-          {{ taskInfo.worker.lastName }}
+        <div class="task-info__title">
+          {{ taskInfo.task.title }}
+        </div>
+        <div class="task-info__description">
+          {{ parsedTaskInfo.description }}
+        </div>
+        <div class="task-info__data-block">
+          <div class="task-info__data">
+            <div class="task-info__parameter">issued:</div>
+            <div class="task-info__value">
+              {{ parsedTaskInfo.createDate }}
+              {{ parsedTaskInfo.createTime }}
+            </div>
+          </div>
+          <div class="task-info__data">
+            <div class="task-info__parameter">deadline:</div>
+            <div class="task-info__value">
+              {{ parsedTaskInfo.deadlineDateAndTime }}
+            </div>
+          </div>
+          <div class="task-info__data">
+            <div class="task-info__parameter">Recurring:</div>
+            <div class="task-info__value">
+              {{ parsedTaskInfo.recurringStatus }}
+            </div>
+          </div>
+          <div class="task-info__data">
+            <div class="task-info__parameter">Result required:</div>
+            <div class="task-info__value">
+              {{ parsedTaskInfo.resultRequiredStatus }}
+            </div>
+          </div>
+          <div class="task-info__data">
+            <div class="task-info__parameter">Status:</div>
+            <div class="task-info__value">
+              done
+            </div>
+          </div>
+        </div>
+        <div class="task-info__buttons">
+          <MyRectangleButton
+            class="task-info__button"
+            icon-name="delete"
+            text="Delete task"
+            @click="deleteModalData.visibilityStatus = true"/>
+          <MyRectangleButton
+            class="task-info__button"
+            icon-name="edit"
+            text="Edit task"/>
         </div>
       </div>
-      <div class="task-info__title">
-        {{ taskInfo.task.title }}
-      </div>
-      <div class="task-info__description">
-        {{ parsedTaskInfo.description }}
-      </div>
-      <div class="task-info__data-block">
-        <div class="task-info__data">
-          <div class="task-info__parameter">issued:</div>
-          <div class="task-info__value">
-            {{ parsedTaskInfo.createDate }}
-            {{ parsedTaskInfo.createTime }}
+      <div class="task-info__history history">
+        <div class="history__title">
+          Task history
+        </div>
+        <div class="history__list">
+          <div
+            class="history__element"
+            v-for="(historyNote, i) in selectedTaskHistory"
+            :key="i">
+            <div class="history__info">
+              <div class="history__date">
+                {{ getDateAndTimeString(historyNote) }}
+              </div>
+              <div class="history__status">
+                {{ getDoneStatus(historyNote) }}
+              </div>
+              <div class="history__in-time">
+                {{ getInTimeStatus(historyNote) }}
+              </div>
+            </div>
+            <div class="history__result">
+              {{ historyNote.result }}
+            </div>
           </div>
         </div>
-        <div class="task-info__data">
-          <div class="task-info__parameter">deadline:</div>
-          <div class="task-info__value">
-            {{ parsedTaskInfo.deadlineDateAndTime }}
-          </div>
-        </div>
-        <div class="task-info__data">
-          <div class="task-info__parameter">Recurring:</div>
-          <div class="task-info__value">
-            {{ parsedTaskInfo.recurringStatus }}
-          </div>
-        </div>
-        <div class="task-info__data">
-          <div class="task-info__parameter">Result required:</div>
-          <div class="task-info__value">
-            {{ parsedTaskInfo.resultRequiredStatus }}
-          </div>
-        </div>
-        <div class="task-info__data">
-          <div class="task-info__parameter">Status:</div>
-          <div class="task-info__value">
-            done
-          </div>
-        </div>
-      </div>
-      <div class="task-info__buttons">
-        <MyRectangleButton
-          class="task-info__button"
-          icon-name="delete"
-          text="Delete task"
-          @click="deleteModalData.visibilityStatus = true"/>
-        <MyRectangleButton
-          class="task-info__button"
-          icon-name="edit"
-          text="Edit task"/>
       </div>
     </div>
 
@@ -74,8 +102,9 @@
 import MyRectangleButton from "@/components/UI/MyRectangleButton";
 import {deleteTask} from "@/assets/js/serverRequest";
 import DeleteModal from "@/components/Modal/DeleteModal";
-import {mapActions} from "vuex";
+import {mapState, mapActions} from "vuex";
 import {parseTaskInfo} from "@/components/TaskBlock/taskLogic";
+import {MyDateClass} from "@/assets/js/microLogic";
 
 export default {
   name: "TaskInfo",
@@ -92,6 +121,9 @@ export default {
     }
   }),
   computed: {
+    ...mapState({
+      selectedTaskHistory: state => state.farms.selectedTaskHistory
+    }),
     parsedTaskInfo() {
       return parseTaskInfo(this.taskInfo.task)
     }
@@ -110,12 +142,43 @@ export default {
 
       this.taskUpdated()
       this.deleteModalData.visibilityStatus = false
+    },
+    getDateAndTimeString(historyNote) {
+      const dateString =
+        MyDateClass.getDateStringByDbValue(historyNote.date)
+      const timeString =
+        MyDateClass.getTimeStringByDbValue(historyNote.time)
+      return MyDateClass.getDateAndTimeString(dateString, timeString)
+    },
+    getDoneStatus(historyNote) {
+      if (historyNote.inTime === 2) {
+        return "not done"
+      }
+      return "done"
+    },
+    getInTimeStatus(historyNote) {
+      if (historyNote.inTime === 2) {
+        return ""
+      }
+
+      return historyNote ? "not in time" : "in time"
     }
   }
 }
 </script>
 
 <style scoped>
+.task-info__content {
+  display: flex;
+}
+
+
+.task-info__task {
+  width: 45%;
+  margin: 0 5% 0 0;
+}
+
+
 .task-info__worker {
   margin: 0 0 30px 0;
 
@@ -187,5 +250,50 @@ export default {
 
 .task-info__button:last-child {
   margin: 0;
+}
+
+
+.task-info__history {
+  width: 50%;
+}
+
+
+.history__title {
+  margin: 0 0 28px 0;
+
+  font-size: 23px;
+
+  color: #333333;
+}
+
+
+.history__element {
+  margin: 0 0 20px 0;
+}
+
+
+.history__info {
+  margin: 0 0 5px 0;
+
+  display: flex;
+
+  font-size: 20px;
+
+  color: #333333;
+}
+
+.history__date {
+  margin: 0 15px 0 0;
+}
+
+.history__status {
+  margin: 0 15px 0 0;
+}
+
+
+.history__result {
+  font-size: 18px;
+
+  color: #555555;
 }
 </style>
